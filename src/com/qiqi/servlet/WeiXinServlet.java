49990e0cceb3c258ef54f10244cb12aae001a5a9
ititellyou <coolqiqi@aliyun.com>
2019-05-10 18:25:23 +0800

    微信公众平台

---
+++ src/com/qiqi/servlet/WeiXinServlet.java
@@ -0,0 +1,43 @@
+package com.qiqi.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.qiqi.util.CheckUtil;
+
+/**
+ * Servlet implementation class WeiXinServlet
+ */
+@WebServlet("/WeiXinServlet")
+public class WeiXinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WeiXinServlet() {
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String signature = request.getParameter("signature");
+		String timestamp = request.getParameter("timestamp");
+		String nonce = request.getParameter("nonce");
+		String echostr = request.getParameter("echostr");
+		
+		PrintWriter out = response.getWriter();
+		if (CheckUtil.checkSignature(signature, timestamp, nonce)) {
+			out.print(echostr);
+		}
+	}
+
+}
