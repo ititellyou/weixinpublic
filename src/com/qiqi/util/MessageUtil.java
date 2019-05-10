7335ce30a343a0748001909e1418e86f6ee00ede
ititellyou <coolqiqi@aliyun.com>
2019-05-10 19:19:59 +0800

    微信公众平台

--- src/com/qiqi/util/MessageUtil.java
+++ src/com/qiqi/util/MessageUtil.java
@@ -1,13 +1,51 @@
 package com.qiqi.util;
 
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.qiqi.po.TextMessage;
+import com.thoughtworks.xstream.XStream;
+
 public class MessageUtil {
-	public static Map<String,String> xmlToMap(HttpServletRequest request){
+	/**
+	 * xml转为map集合
+	 * @param request
+	 * @return
+	 * @throws Exception
+	 */
+	public static Map<String,String> xmlToMap(HttpServletRequest request) throws Exception{
 		Map<String, String> map = new HashMap<String, String>();
+		SAXReader reader = new SAXReader();
+		InputStream ins = request.getInputStream();
+		Document doc = reader.read(ins);
+		
+		Element root = doc.getRootElement();
+		
+		List<Element> list = root.elements();
+		
+		for (Element element : list) {
+			map.put(element.getName(), element.getText());
+		}
+		
+		ins.close();
 		return map;
+	}
+	
+	/**
+	 * 文本消息转换为xml对象
+	 * @param textMessage
+	 * @return
+	 */
+	public static String textMessageToXml(TextMessage textMessage) {
+		XStream xStream = new XStream();
+		return xStream.toXML(textMessage);
 	}
 }
