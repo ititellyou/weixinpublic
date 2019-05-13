a8b7d88fa4a1a656f1f4c0a40f0d4230b71a94d4
coolqiqi@aliyun.com <QIQI@QIQISaviour>
2019-05-13 22:13:24 +0800

    公众平台

--- src/com/qiqi/util/MessageUtil.java
+++ src/com/qiqi/util/MessageUtil.java
@@ -15,6 +15,18 @@ import com.qiqi.po.TextMessage;
 import com.thoughtworks.xstream.XStream;
 
 public class MessageUtil {
+	
+	public static final String MESSAGE_TEXT = "text";
+	public static final String MESSAGE_image = "image";
+	public static final String MESSAGE_VOICE = "voice";
+	public static final String MESSAGE_VIDEO = "video";
+	public static final String MESSAGE_LINK = "link";
+	public static final String MESSAGE_LOCATION = "location";
+	public static final String MESSAGE_EVENT = "event";
+	public static final String MESSAGE_SUBSCRIBE = "subscribe";
+	public static final String MESSAGE_UNSUBSCRIBE = "unsubscribe";
+	public static final String MESSAGE_CLICK = "CLICK";
+	public static final String MESSAGE_VIEW = "VIEW";
 	/**
 	 * xml转为map集合
 	 * @param request
